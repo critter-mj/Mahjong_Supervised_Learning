1e8455a79ea09bdbd70ff0b81a4ee1db899b84a9
critter-mj <mkmjai1@gmail.com>
2020-04-13 13:49:49 +0900

    initial commit

---
+++ discard.py
@@ -0,0 +1,118 @@
+import glob
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+import argparse
+
+from util import *
+
+IN_CHANNELS = 240
+#MID_CHANNELS = 256
+MID_CHANNELS = 32
+BLOCKS_NUM = 10
+BATCH_SIZE = 32
+#BATCH_SIZE = 2
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class Trainer:
+    def __init__(self, model, optimizer, criterion):
+        self.model = model.to(DEVICE)
+        self.optimizer = optimizer
+        self.criterion = criterion
+
+    def set_file_list(self, train_prefix, test_prefix):
+        self.train_file_list = glob.glob(train_prefix)
+        self.test_file_list = glob.glob(test_prefix)
+
+    def epoch(self, is_train):
+        if is_train:
+            self.model.train()
+            file_list = self.train_file_list
+        else:
+            self.model.eval()
+            file_list = self.test_file_list
+        epoch_loss = 0
+        correct = 0
+        total = 0
+
+        for file_path in tqdm(file_list):
+            file_data = FileDatasetsAug(file_path)
+            data_loader = DataLoader(file_data, batch_size=BATCH_SIZE, shuffle=is_train, drop_last=is_train)
+
+            for inputs, targets in data_loader:
+                inputs = torch.unsqueeze(inputs.to(DEVICE).float(), -1)
+                targets = targets.to(DEVICE).long()
+
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, targets)
+
+                if is_train:
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                epoch_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                correct += predicted.eq(targets.data).cpu().sum().item()
+                total += targets.size(0)
+
+        epoch_loss /= total
+        acc = 100 * correct / total
+        return epoch_loss, acc
+
+    def run_train(self, epoch_begin, epoch_end, test_interval, save_interval):
+        for i in tqdm(range(epoch_begin, 1 + epoch_end)):
+            train_loss, train_acc = self.epoch(True)
+            print("train_loss:", train_loss, "train_acc:", train_acc)
+
+            if i % test_interval == 0 and 0 < len(self.test_file_list):
+                test_loss, test_acc = self.epoch(False)
+                print("test_loss:", test_loss, "test_acc:", test_acc)
+
+            if i % save_interval == 0:
+                state = {'epoch': i, 'model': self.model.state_dict(), 'optimizer': self.optimizer.state_dict()}
+                torch.save(state, "train_tmp.pth")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load', action='store_true')
+    args = parser.parse_args()
+
+    model = DiscardNet(IN_CHANNELS, MID_CHANNELS, BLOCKS_NUM)
+    
+    criterion = nn.CrossEntropyLoss()
+    
+    
+    if args.load:
+        tmp = torch.load("train_tmp.pth")
+        model.load_state_dict(tmp['model'])
+        optimizer = torch.optim.Adam(model.parameters())
+        optimizer.load_state_dict(tmp['optimizer'])
+        if DEVICE == 'cuda':
+            for state in optimizer.state.values():
+                for k, v in state.items():
+                    if isinstance(v, torch.Tensor):
+                        state[k] = v.cuda()
+        epoch_begin = tmp['epoch'] + 1
+    else:
+        optimizer = torch.optim.Adam(model.parameters())
+        epoch_begin = 0
+
+    trainer = Trainer(model, optimizer, criterion)
+
+    train_prefix = "../akochan_ui/tenhou_npz/discard/2018/20180101/discard_*.npz"
+    #train_prefix = "tenhou_npz/discard/2018/20180101/discard_*.npz"
+    test_prefix = "../akochan_ui/tenhou_npz/discard/2018/20180110/discard_*.npz"
+    #test_prefix = "tenhou_npz/discard/2018/20180102/discard_*.npz"
+    trainer.set_file_list(train_prefix, test_prefix)
+
+    print(trainer.test_file_list)
+
+    trainer.run_train(epoch_begin, 200, 1, 10)
+
---
+++ util.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+""" network model """
+
+"""
+def get_tmp_size(models, input_size):
+    tmp = (BATCH_SIZE, input_size[0], input_size[1], input_size[2])
+    x = torch.zeros(tmp)
+    for model in models:
+        x = model(x)
+    return tuple([x.size()[i] for i in range(1, x.dim())])
+"""
+
+class ResBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResBlock, self).__init__()
+        self.sequential = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=(3,1), padding=(1,0), bias=False),
+            #nn.BatchNorm2d(planes),
+            nn.ReLU(),
+            nn.Conv2d(channels, channels, kernel_size=(3,1), padding=(1,0), bias=False),
+            #nn.BatchNorm2d(planes)
+        )
+
+    def forward(self, x):
+        x = self.sequential(x) + x
+        return F.relu(x)
+
+class DiscardNet(nn.Module):
+    def __init__(self, in_channels, channels_num, blocks_num):
+        super(DiscardNet, self).__init__()
+        self.preproc = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=channels_num, kernel_size=(3,1), padding=(1,0), bias=False),
+            #nn.BatchNorm2d(self.channels[0]),
+            nn.ReLU()
+        )
+
+        blocks = []
+        for _i in range(blocks_num):
+            blocks.append(ResBlock(channels_num))
+        self.res_blocks = nn.Sequential(*blocks)
+
+        self.postproc = nn.Sequential(
+            nn.Conv2d(in_channels=channels_num, out_channels=1, kernel_size=(3,1), padding=(1,0), bias=False),
+            #nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.preproc(x)
+        x = self.res_blocks(x)
+        x = self.postproc(x)
+        x = x.view(x.size(0), -1)  # [B, C, H, W] -> [B, C*H*W]
+        return x
+
+""" dataset """
+
+class FileDatasets(Dataset):
+    def __init__(self, file_path):
+        npz = np.load(file_path)
+        self.data = npz['arr_0']
+        self.label = npz['arr_1']
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, idx):
+        return self.data[idx], np.argmax(self.label[idx])
+
+class Hai34Transform():
+    def __init__(self):
+        pass
+
+    def swap_color(self, x, c1, c2, c3):
+        for i in range(9):
+            x[[i, 9+i, 18+i]] = x[[c1*9+i, c2*9+i, c3*9+i]]
+
+    def swap_sangen(self, x, c1, c2, c3):
+        pos = 31
+        x[[pos, pos+1, pos+2]] = x[[pos+c1, pos+c2, pos+c3]]
+
+    def reverse_num(self, x):
+        for i in range(4):
+            x[[i, 8-i, 9+i, 17-i, 18+i, 26-i]] = x[[8-i, i, 17-i, 9+i, 26-i, 18+i]]
+
+    def random_trans(self, x):
+        i_color = np.random.randint(6)
+        i_sangen = np.random.randint(6)
+        i_rev = np.random.randint(2)
+
+        if i_color == 1:
+            self.swap_color(x, 0, 2, 1)
+        elif i_color == 2:
+            self.swap_color(x, 1, 0, 2)
+        elif i_color == 3:
+            self.swap_color(x, 1, 2, 0)
+        elif i_color == 4:
+            self.swap_color(x, 2, 0, 1)
+        elif i_color == 5:
+            self.swap_color(x, 2, 1, 0)
+
+        if i_sangen == 1:
+            self.swap_sangen(x, 0, 2, 1)
+        elif i_sangen == 2:
+            self.swap_sangen(x, 1, 0, 2)
+        elif i_sangen == 3:
+            self.swap_sangen(x, 1, 2, 0)
+        elif i_sangen == 4:
+            self.swap_sangen(x, 2, 0, 1)
+        elif i_sangen == 5:
+            self.swap_sangen(x, 2, 1, 0)
+
+        if i_rev == 1:
+            self.reverse_num(x)
+
+class FileDatasetsAug(Dataset):
+    def __init__(self, file_path):
+        npz = np.load(file_path)
+        self.data = npz['arr_0']
+        self.label = npz['arr_1']
+        self.trans = Hai34Transform()
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, idx):
+        tmp = np.vstack((self.data[idx], self.label[idx]))
+        tmp = np.transpose(tmp)
+        self.trans.random_trans(tmp)
+        tmp = np.transpose(tmp)
+
+        return tmp[:-1], np.argmax(tmp[-1])
